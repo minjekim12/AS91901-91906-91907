bf4120ec1472bb3beca7971a3427ac1868389cc8
Minje Kim <minjekim@Minjeui-MacBookAir.local>
2023-08-22 19:16:40 +1200

    tutorial page created. cannot exit from tutorial page yet

--- main.py
+++ main.py
@@ -43,15 +43,27 @@ root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 # Main Menu Background Image
 mainmenu_open = Image.open('resources/main_menu_bg.png') # Opens Image
 mainmenu_bg = ImageTk.PhotoImage(mainmenu_open) # Assign into a ImageTk.Photoimage widget. 
+main_menu_bg_label = Label(root, image=mainmenu_bg) # Assign into a Tkinter Label
 
 # Tutorial Page Background Image
 tutorial_open = Image.open('resources/tutorial_bg.png') # Opens Image
 tutorial_bg = ImageTk.PhotoImage(tutorial_open) # Assign into a ImageTk.Photoimage widget.
+tutorial_bg_label = Label(root, image=tutorial_bg) # Assign into a Tkinter Label
+
+
+# ------------------------------ TUTORIAL PAGE --------------------------------
+def TUTORIALPAGE():
+    main_menu_bg_label.destroy()
+    game_description_label.destroy()
+    tutorial_button.destroy()
+    game_start_button.destroy()
+    settings_button.destroy()
+    tutorial_bg_label.place(x = 0, y = 0)
+
 
 # --------------------------------- MAIN MENU ------------------------------------
 
 # Display Main Menu Background Image
-main_menu_bg_label = Label(root, image=mainmenu_bg)
 main_menu_bg_label.place(x = 0,y = 0)
 
 # Add text
@@ -59,7 +71,7 @@ game_description_label = Label(root, text = "FILL IN THE LETTERS TO MAKE AS MANY
                font=("DIN Alternate", 24), fg=("#1b3652"), bg=("white"), padx=15, pady=3)
 game_description_label.place(x=240, y=340)
 
-tutorial_button = Button(root, text="TUTORIAL", font=("DIN Alternate", 24), fg=("#1b3652"), bg=("white"), padx=7, pady=9, borderwidth=0) #command=TUTORIALPAGE)
+tutorial_button = Button(root, text="TUTORIAL", font=("DIN Alternate", 24), fg=("#1b3652"), bg=("white"), padx=7, pady=9, borderwidth=0, command=TUTORIALPAGE)
 tutorial_button.place(x=240, y=420)
 
 game_start_button = Button(root, text="PLAY", font=("DIN Alternate", 24), fg=("#1b3652"), bg=("white"), padx=29, pady=9, borderwidth=0)
@@ -71,7 +83,6 @@ settings_button.place(x=555, y=420)
 
 
 
-
-# ------------------ EXECUTE PYTHON -
+# ------------------ EXECUTE PYTHON -------------------
 # Runs the program and loops to keep it open until closed
 root.mainloop() 
